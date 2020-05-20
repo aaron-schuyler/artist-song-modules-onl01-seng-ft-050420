f2f5e465c47a263dbe3e3e32d8a66b6e82419b8e
Aaron Schuyler <asky28@gmail.com>
2020-05-20 01:46:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,15 +18,15 @@ class Artist
   def self.all
     @@artists
   end
-
+=begin
   def self.reset_all
     self.all.clear
   end
-
+=end
   def self.count
     self.all.count
   end
-
+=end
   def add_song(song)
     @songs << song
     song.artist = self
